4e3b09d57728e409e0d8e39897b13a78c5aad1d0
Frederik "Freso" S. Olesen <freso.dk@gmail.com>
2013-01-13 22:22:58 +0100

    Use HTML (5) instead of XHTML (1.0).

--- plugin.py
+++ plugin.py
@@ -80,10 +80,8 @@ def html_start(channel, date):
         'channel': channel,
         'date': time.strftime(dateformat, date),
     })
-    html = """<?xml version="1.1" encoding="UTF-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+    html = """<!DOCTYPE html>
+<html>
 <head>
  <title>{title}</title>
  <link rel="stylesheet" href="/musicbrainz/style.css" type="text/css" />
